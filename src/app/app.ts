995cde5eb3932ee940f9e34d97307b809c0449c0
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-05 13:02:15 +0500

    first lesson for angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarList } from "./car-list/car-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, CarList],
+  template: `<app-car-list></app-car-list>
+  <h3 class="paragraph">Hello ddim sanga</h3>`,
 })
 export class App {
   protected title = 'angular-app';
