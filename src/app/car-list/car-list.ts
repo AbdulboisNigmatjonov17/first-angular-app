6611a0aa04a72466463142b8bd9a61063ae9dabc
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-06 14:57:07 +0500

    one/two way data binding

--- src/app/car-list/car-list.ts
+++ src/app/car-list/car-list.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Car } from '../models/car';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-car-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './car-list.html',
   styleUrl: './car-list.css'
 })
-export class CarList {
+// one way data binding
+// two way data binding
 
+export class CarList {
+  carName: string = 'Bmw'
+  carYear: Date = new Date()
+
+  cars: Car[] = [
+    { id: 1, name: 'Bmw', year: new Date() },
+  ]
+
+  onSubmit() {
+    if (this.carName.length != 0 && this.carYear) {
+      const newCar: Car = {
+        id: this.cars.length + 1,
+        name: this.carName,
+        year: this.carYear
+      }
+      this.cars.push(newCar)
+      this.carName = ''
+      this.carYear = new Date()
+      console.log(this.cars);
+      
+    }
+  }
 }
