542e3ed9ad53263ab47d4ada530793edf9b12df5
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-06 17:59:51 +0500

    fourth lesson

--- src/app/car-list/car-list.ts
+++ src/app/car-list/car-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from '../models/car';
 import { FormsModule } from '@angular/forms';
 
@@ -11,14 +11,18 @@ import { FormsModule } from '@angular/forms';
 // one way data binding
 // two way data binding
 
-export class CarList {
+export class CarList implements OnInit {  
   carName: string = 'Bmw'
   carYear: Date = new Date()
-
+  
   cars: Car[] = [
     { id: 1, name: 'Bmw', year: new Date() },
   ]
-
+  
+  ngOnInit(): void {
+    const savedCars = localStorage.getItem('cars')
+    savedCars?this.cars = JSON.parse(savedCars): this.cars = []
+  }
   onSubmit() {
     if (this.carName.length != 0 && this.carYear) {
       const newCar: Car = {
@@ -29,8 +33,11 @@ export class CarList {
       this.cars.push(newCar)
       this.carName = ''
       this.carYear = new Date()
-      console.log(this.cars);
-      
+      localStorage.setItem('cars', JSON.stringify(this.cars))
     }
+  }
+  onDelete(car: Car) {
+    this.cars = this.cars.filter(c => c.id != car.id)
+    localStorage.setItem('cars', JSON.stringify(this.cars))
   }
 }
