33ff72e451bc300cf9ff6f815e2573471b7de6c3
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-12 09:59:54 +0500

    signals with forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { UserComponent } from './user/user';
 
 @Component({
@@ -7,5 +7,19 @@ import { UserComponent } from './user/user';
   templateUrl: './app.html',
 })
 export class App {
+  email = signal('')
 
+  constructor() {
+    effect(() => {
+      const keyword = this.email()
+      if (keyword.length >= 3) {
+        console.log(`Search for ${keyword}`);
+      }
+    })
+  }
+
+  onEmailChange(event: Event) {
+    const input = event.target as HTMLInputElement
+    this.email.set(input.value)
+  }
 }
