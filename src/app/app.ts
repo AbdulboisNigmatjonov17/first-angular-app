c5f0afe87cd9e43e8294f1116903f42c67a3c976
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-12 11:55:15 +0500

    deferrable view

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal, effect } from '@angular/core';
 import { UserComponent } from './user/user';
+import { Comments } from './comments/comments';
+import { Article } from './article/article';
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent],
+  imports: [UserComponent, Comments, Article],
   templateUrl: './app.html',
 })
 export class App {
@@ -23,3 +25,7 @@ export class App {
     this.email.set(input.value)
   }
 }
+
+// @defer - Bu asosiy block. Component ni kechiktirib yuklaydi.
+// @placeholder - Yuklash vaqtidan oldn ko'rsatiladigan joy.
+// @loading - Yuklanayotgan paytda ko'rsatiladigan joy.
