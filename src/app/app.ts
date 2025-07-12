138074229efc544175cd690a8785cc99d534979f
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-12 09:07:47 +0500

    otadan bolaga malumot uzatish ya'ni props

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,13 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CarList } from "./car-list/car-list";
-import { CurrencyPipe } from '@angular/common';
+import { Component, } from '@angular/core';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CarList, CurrencyPipe],
-  templateUrl: './app.html',
+  imports: [User],
+  template: `<section>
+    <app-user [userName]="userName"></app-user>
+  </section>`,
 })
 export class App {
-  count = signal(0);
-
-  constructor() {
-    effect(() => console.log('count', this.count()))
-  }
-  increment() {
-    this.count.set(this.count() + 1);
-  }
-  decrement() {
-    this.count.set(this.count() - 1);
-  }
-  reset() {
-    this.count.set(0);
-  }
+  userName: string = 'CostaRICO'
 }
-// signal() - Qiymat saqlidi,
-// computed() - Signal asosida avtomatik hisoplangan qiymat,
-// effect() - Signal ozgarishlarini kuzatadi va qayta ishlidi.
