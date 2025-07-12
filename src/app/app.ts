4f213260f27508a164d0215976c71d2d03ff559e
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-12 15:06:53 +0500

    image optimization

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,17 @@ import { Component, signal, effect } from '@angular/core';
 import { UserComponent } from './user/user';
 import { Comments } from './comments/comments';
 import { Article } from './article/article';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent, Comments, Article],
+  imports: [UserComponent, Comments, Article, NgOptimizedImage],
   templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
 export class App {
   email = signal('')
+  preiviewImage: string = 'https://carapandang.com/uploads/news/CJOvZM5mHFJMCuAK9JAkV3o6FXuZfiZHWzvRJ8oy.jpg'
 
   constructor() {
     effect(() => {
